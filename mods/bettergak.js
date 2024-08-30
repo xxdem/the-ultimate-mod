914f23934adbfe6426e8ec298cd4ffffc9d8bf56
dem <69117597+xxdem@users.noreply.github.com>
2024-08-30 12:00:01 -0400

    bug relating to changing gakvu's dialogue portrait

--- mods/bettergak.js
+++ mods/bettergak.js
@@ -9,11 +9,17 @@ MODIFIED BY : max :ᶅ (@the_dem on discord)
 */
 
 
-content.insertAdjacentHTML('beforeend', `<link type="text/css" rel="stylesheet" href="https://pocl.vip/corru/bettergak.css">`);
+function changeGakvu() {
+    content.insertAdjacentHTML('beforeend', `<link type="text/css" rel="stylesheet" href="https://pocl.vip/corru/bettergak.css">`);
 
-env.dialogueActors.gakvu.image = "https://pocl.vip/corru/bettergak/portrait.gif";
+    env.dialogueActors.gakvu.image = "https://pocl.vip/corru/bettergak/portrait.gif";
 
-env.COMBAT_ACTORS.gakvu.portrait = `<img class="portrait" src="https://pocl.vip/corru/bettergak/gakattak.gif">`;
-env.COMBAT_ACTORS.gakvu.portraitUrl = 'https://pocl.vip/corru/bettergak/gakattak.gif';
+    env.COMBAT_ACTORS.gakvu.portrait = `<img class="portrait" src="https://pocl.vip/corru/bettergak/gakattak.gif">`;
+    env.COMBAT_ACTORS.gakvu.portraitUrl = 'https://pocl.vip/corru/bettergak/gakattak.gif';
 
-env.entities['gakvu'].image = 'https://pocl.vip/corru/bettergak/portrait.gif'
+    env.entities['gakvu'].image = 'https://pocl.vip/corru/bettergak/portrait.gif'
+
+    document.addEventListener('corru_entered', changeGakvu, true)
+}
+
+document.addEventListener('corru_entered', changeGakvu, true)
