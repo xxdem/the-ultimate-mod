62666ba107ae743430f1179741b6373bf5df4135
dem <69117597+xxdem@users.noreply.github.com>
2024-08-30 11:54:12 -0400

    added mod links

--- loader.js
+++ loader.js
@@ -4,7 +4,7 @@
 the mega-modpack featuring nearly ALL mods created for corru.observer, to be loaded all at once
 
 CREDITS:
-    More Decline Buttons, More Velzie's glee, Vs. Call Research, Fintrusive
+    More Decline Buttons, More Velzie's glee, Vs. Call Research, Fintrusive, Missing Entities, Overcoherent Waters
         BY: adr (@adr_furret on discord)
     Quieter Masks, Stowaway Unity
         BY: agent7 (@agent7 on discord)
@@ -16,26 +16,44 @@ CREDITS:
         BY: pocl.v (@pocl.v on discord)
 
 
-NOTICE :: 'temporary template loader put in place'
+NOTICE :: '10 of 14 mods added'
+PLANS :: 'sys menu thing that makes 100% velz gele, doubled, and quieter masks a menu setting'
 */
 
 
 // LOADER
     // page specific mods
 switch(page.path) {
-    case "/local/ocean/embassy/": addResources(['']); break;
-    case "/local/ocean/embassy/golem/": addResources(['']); break;
+    case "/hub/": addResources(["https://file.garden/ZBykMtEMpVTUWZ-e/ULTIMATE_MODPACK/hammer_funf.js"]); break;
+    case "/local/ocean/": addResources(["https://adrfurret.neocities.org/corrumods/overcoherentwaters.js"]); break;
+    case "/local/ocean/embassy/": addResources(["https://file.garden/ZBykMtEMpVTUWZ-e/ULTIMATE_MODPACK/bettergak.js"]); break;
+    case "/local/ocean/embassy/golem/": addResources(["https://file.garden/ZBykMtEMpVTUWZ-e/ULTIMATE_MODPACK/bettergak.js"]); break;
     case "/local/beneath/embassy/":addResources(["https://adrfurret.neocities.org/corrumods/e3a2_decline.js",
                                                 "https://adrfurret.neocities.org/corrumods/e3a2_morevelziesgleestatuses.js",
                                                 "https://adrfurret.neocities.org/corrumods/e3a2_callresearch.js",
-                                                "https://adrfurret.neocities.org/corrumods/e3a2_fintrusive.js",]); break;
+                                                "https://adrfurret.neocities.org/corrumods/e3a2_fintrusive.js",
+                                                "https://adrfurret.neocities.org/corrumods/e3a2_missingenemies.js"]); break;
 }
+
+    // ones that runs anywhere
+addResources(['https://file.garden/ZBykMtEMpVTUWZ-e/ULTIMATE_MODPACK/random_velzie.js'])
 
 
 // EVENT LISTENER
 document.addEventListener('corru_loaded', ()=>{
+        // page specific mods
     switch(page.path) {
-        case "/local/ocean/embassy/": addResources(['']); break;
-        case "/local/ocean/embassy/golem/": addResources(['']); break;
+        case "/hub/": addResources(["https://file.garden/ZBykMtEMpVTUWZ-e/ULTIMATE_MODPACK/hammer_funf.js"]); break;
+        case "/local/ocean/": addResources(["https://adrfurret.neocities.org/corrumods/overcoherentwaters.js"]); break;
+        case "/local/ocean/embassy/": addResources(["https://file.garden/ZBykMtEMpVTUWZ-e/ULTIMATE_MODPACK/bettergak.js"]); break;
+        case "/local/ocean/embassy/golem/": addResources(["https://file.garden/ZBykMtEMpVTUWZ-e/ULTIMATE_MODPACK/bettergak.js"]); break;
+        case "/local/beneath/embassy/":addResources(["https://adrfurret.neocities.org/corrumods/e3a2_decline.js",
+                                                    "https://adrfurret.neocities.org/corrumods/e3a2_morevelziesgleestatuses.js",
+                                                    "https://adrfurret.neocities.org/corrumods/e3a2_callresearch.js",
+                                                    "https://adrfurret.neocities.org/corrumods/e3a2_fintrusive.js",
+                                                    "https://adrfurret.neocities.org/corrumods/e3a2_missingenemies.js"]); break;
     }
+
+        // ones that runs anywhere
+    addResources(['https://file.garden/ZBykMtEMpVTUWZ-e/ULTIMATE_MODPACK/random_velzie.js'])
 })
